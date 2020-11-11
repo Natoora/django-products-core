e6fe81c004cbba42166b0cfa642e7da68ec01cbb
angelinerj <angelinepuspa@hotmail.com>
2020-11-11 16:10:41 +0000

    MANIFEST.in + Files underneath product-core + some code in models.py and apps.py

---
+++ products_core/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProductsCore(AppConfig):
+    name = 'products_core'
--- products_core/models.py
+++ products_core/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+import uuid
 
 
 class ProductsCore(models.Model):
-    name = models.CharField(max_Field = 100)
-    status = models.CharField(max_Field = 20)
+    Active = 'Active'
+    name = models.CharField(max_Field=100)
+    status = models.CharField(max_Field=20)
+    # id = models.UUIDField(primary_key=Tr)
     abstract = True
