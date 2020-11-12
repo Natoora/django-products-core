4c9a27db712ce6221decbef85cba334f1a2ad0f1
VMihai-dev <iammihaivisan@gmail.com>
2020-11-12 10:51:10 +0000

    Renamen Status to ProductStatusChoice and added questions at the buttom.

--- products_core/models.py
+++ products_core/models.py
@@ -2,16 +2,19 @@ import uuid
 from django.db import models
 
 
-class Status(models.TextChoices):
+class ProductStatusChoice(models.TextChoices):
     ACTIVE = 'ACTIVE', 'Active'
     DISABLED = 'DISABLED', 'Disabled'
     DISCONTINUED = 'DISCONTINUED', 'Discontinued'
 
 
 class ProductCore(models.Model):
-
+    """
+    ProductCore Abstract Model
+    """
     name = models.CharField(max_length=100)
-    status = models.CharField(max_length=20, choices=Status.choices, default=Status.ACTIVE, db_index=True)
+    status = models.CharField(max_length=20, choices=ProductStatusChoice.choices,
+                              default=ProductStatusChoice.ACTIVE, db_index=True)
     uuid_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     # This code below function is here for testing
@@ -26,6 +29,17 @@ class ProductBaseCore(models.Model):
     """
     ProductBaseCore Abstract Model
     """
+    status = models.CharField(max_length=20, choices=ProductStatusChoice.choices,
+                              default=ProductStatusChoice.ACTIVE, db_index=True)
+    name = models.CharField(max_length=100, unique=True)
+    uuid_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    class Meta:
+        abstract = True
+
+
+"""
+    OLD STYLE TUPLE
     ACTIVE = 'ACTIVE'
     DISABLED = 'DISABLED'
     DISCONTINUED = 'DISCONTINUED'
@@ -35,10 +49,9 @@ class ProductBaseCore(models.Model):
         (DISABLED, 'Disabled'),
         (DISCONTINUED, 'Discontinued'),
     )
+"""
 
-    status = models.CharField(max_length=20, choices=PRODUCT_STATUS_CHOICES, default=ACTIVE)
-    name = models.CharField(max_length=100, unique=True)
-    uuid_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-
-    class Meta:
-        abstract = True
+# Questions:
+# 1. Do we need to use the same list of choices for both statuses in the 2 classes?
+# 2. If no, do we use the tuple in the ss for the ProductBaseCore
+# 3. If yes, can we not inherit the fields from ProductCore in ProductBaseCore ? (use only 1 class?)
