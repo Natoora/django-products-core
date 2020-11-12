ffde67f482eaffd995061be0b4dbb08406ff7835
angelinerj <angelinepushpa@hotmail.com>
2020-11-12 10:34:42 +0000

    created a status class and cleaned up code

--- products_core/models.py
+++ products_core/models.py
@@ -1,14 +1,19 @@
-from django.db import models
 import uuid
+from django.db import models
 
+
+class Status(models.TextChoices):
+    ACTIVE = 'ACTIVE', 'Active'
+    DISABLED = 'DISABLED', 'Disabled'
+    DISCONTINUED = 'DISCONTINUED', 'Discontinued'
 
 class ProductCore(models.Model):
 
     name = models.CharField(max_Field=100)
-    status = models.CharField(max_Field=20)
+    status = models.CharField(max_length=2, choices=Status.choices,default=Status.ACTIVE,db_index=True)
+    uuid_code = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
+    # This code below function is here for testing
     def __str__(self):
         return self.name
 
