b849368a9eff7a5091082d75e68338bbd3491a60
angelinerj <angelinepuspa@hotmail.com>
2020-11-12 18:04:11 +0000

    Made changes to model.py

--- products_core/models.py
+++ products_core/models.py
@@ -2,40 +2,37 @@ import uuid
 from django.db import models
 
 
-class ProductStatusChoice(models.TextChoices):
-    ACTIVE = 'ACTIVE', 'Active'
-    DISABLED = 'DISABLED', 'Disabled'
-    DISCONTINUED = 'DISCONTINUED', 'Discontinued'
+class ProductCommon(models.Model):
 
-
-class ProductCore(models.Model):
-    """
-    ProductCore Abstract Model
-    """
-    name = models.CharField(max_length=100)
+    class ProductStatusChoice(models.TextChoices):
+        ACTIVE = 'ACTIVE', 'Active'
+        DISABLED = 'DISABLED', 'Disabled'
+        DISCONTINUED = 'DISCONTINUED', 'Discontinued'
     status = models.CharField(max_length=20, choices=ProductStatusChoice.choices,
                               default=ProductStatusChoice.ACTIVE, db_index=True)
     uuid_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    class Meta:
+        abstract = True
+
+
+class ProductCore(ProductCommon):
+    """
+    ProductCore Abstract Model
+    """
+
+    name = models.CharField(max_length=100)
 
     # This code below function is here for testing
     def __str__(self):
         return self.name
-    
-    class Meta:
-        abstract = True
 
 
-class ProductBaseCore(models.Model):
+class ProductBaseCore(ProductCommon):
     """
     ProductBaseCore Abstract Model
     """
-    status = models.CharField(max_length=20, choices=ProductStatusChoice.choices,
-                              default=ProductStatusChoice.ACTIVE, db_index=True)
     name = models.CharField(max_length=100, unique=True)
-    uuid_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-
-    class Meta:
-        abstract = True
 
 
 """
