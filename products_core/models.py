191df1c1c5de533bb27830ee56fd1693167895c9
VMihai-dev <iammihaivisan@gmail.com>
2020-11-12 10:14:37 +0000

    Comment

--- products_core/models.py
+++ products_core/models.py
@@ -3,14 +3,19 @@ import uuid
 
 
 class ProductBaseCore(models.Model):
+    """
+    ProductBaseCore Abstract Model
+    """
     ACTIVE = 'ACTIVE'
     DISABLED = 'DISABLED'
     DISCONTINUED = 'DISCONTINUED'
+
     PRODUCT_STATUS_CHOICES = (
         (ACTIVE, 'Active'),
         (DISABLED, 'Disabled'),
         (DISCONTINUED, 'Discontinued'),
     )
+
     status = models.CharField(max_length=20, choices=PRODUCT_STATUS_CHOICES, default=ACTIVE)
     name = models.CharField(max_length=100, unique=True)
     uuid_code = models.UUIDField(default=uuid.uuid4(), editable=False, unique=True)
