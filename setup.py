250fbebd63907b746e1d5b7b93f91907a7b0ff06
angelinerj <angelinepushpa@hotmail.com>
2020-11-17 19:08:04 +0000

    changes to setup.cfg and setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup
 
-setup()
+setup(
+    python_requires="=>3.6",
+    install_requires=[
+        "Django==3.1.3",
+        "djangorestframework==3.11.1",
+        "factory_boy==3.1.0",
+        "faker==4.14.1" "black==20.8b1",
+    ],
+)
