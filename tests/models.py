34f988f4cf2dddc133eccbb1c4d69d5c8a3d81eb
Mihai <59050660+VMihai-dev@users.noreply.github.com>
2020-11-19 11:16:29 +0000

    Update models.py

--- tests/models.py
+++ tests/models.py
@@ -3,8 +3,8 @@ from products_core.models import ProductCore, ProductBaseCore
 
 
 class ProductCore(ProductCore):
-    custom_att = models.CharField(max_length=200, default="123")
+    custom_att = models.IntegerField()
 
 
 class ProductBaseCore(ProductBaseCore):
-    custom_att = models.CharField(max_length=200, default="123")
+    custom_att = models.IntegerField()
