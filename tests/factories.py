3eecd0294b0f2b4fc0748b2fde2980c147a7d650
Mihai <59050660+VMihai-dev@users.noreply.github.com>
2020-11-19 11:15:55 +0000

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -1,10 +1,9 @@
-from faker import Factory
 from tests.models import ProductCore, ProductBaseCore
 from products_core.factories import ProductCoreFactory
-
-faker = Factory.create()
 
 
 class ProductFactory(ProductCoreFactory):
     class Meta:
         model = ProductCore
+
+     
