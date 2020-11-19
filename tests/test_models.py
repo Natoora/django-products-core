ba692773fe06d56b0db03b509189f87e7be83ee9
Mihai <59050660+VMihai-dev@users.noreply.github.com>
2020-11-19 11:24:42 +0000

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -21,33 +21,36 @@ class ProductCoreModelTests(TestCase):
     """ Product Core TestCases """
 
     def setUp(self):
-        self.product = ProductCoreFactory(name="foobar", uuid_code="c532c472-cb23-4c91-994e-36ca1da8b71e")
+        self.product = ProductCoreFactory.create(
+            uuid_code="c532c472-cb23-4c91-994e-36ca1da8b71e",
+            status=Customer.StatusChoices.ACTIVE,
+            name="foobar",
+            custom_att=10,
+        )
         logger.info('Finished setUp: Created ProductCore object: {} - {}'.format(self.product.name, self.product.status))
 
     #
     # Attributes from Abstract Model
     #
-
-    # name
-    def test_name(self):
-        logger.info('Doing the test_name test')
-        self.assertEqual(self.product.name, "foobar")
-
-    def test_name_max_length(self):
-        logger.info('Doing the test_name_max_length test')
-        exceed_limits = "".join(
-            random.choices(string.ascii_letters + string.digits, k=201)
+    
+    # uuid_code
+    def test_uuid_code(self):
+        self.assertEqual(
+            self.product.uuid_code, "c532c472-cb23-4c91-994e-36ca1da8b71e"
         )
-        with self.assertRaises(ValidationError):
-            self.product.name = exceed_limits
-            self.product.full_clean()  # calls save()
 
-    def test_name_not_null(self):
-        logger.info('Doing the test_name_not_null test')
-        with self.assertRaises(ValidationError):
-            self.product.name = None
-            self.product.full_clean()  # calls save()
+    def test_uuid_code_is_not_editable(self):
+        # Not possible to test as editable=False only applies to Django Admin
+        pass
 
+    def test_uuid_code_unique(self):
+        with self.assertRaises(IntegrityError):
+            ProductCoreFactory.create(uuid_code="c532c472-cb23-4c91-994e-36ca1da8b71e")
+
+    def test_uuid_code_not_null(self):
+        with self.assertRaises(IntegrityError):
+            ProductCoreFactory.create(uuid_code=None)
+         
     # status
     def test_status(self):
         self.assertEqual(self.product.status, self.product.ProductStatusChoice.ACTIVE)
@@ -70,21 +73,43 @@ class ProductCoreModelTests(TestCase):
     def test_status_not_null(self):
         with self.assertRaises(IntegrityError):
             ProductCoreFactory.create(status=None)
+            
+    # name
+    def test_name(self):
+        logger.info('Doing the test_name test')
+        self.assertEqual(self.product.name, "foobar")
 
-    # uuid_code
-    def test_uuid_code(self):
-        self.assertEqual(
-            self.product.uuid_code, "c532c472-cb23-4c91-994e-36ca1da8b71e"
+    def test_name_max_length(self):
+        logger.info('Doing the test_name_max_length test')
+        exceed_limits = "".join(
+            random.choices(string.ascii_letters + string.digits, k=201)
         )
+        with self.assertRaises(ValidationError):
+            self.product.name = exceed_limits
+            self.product.full_clean()  # calls save()
 
-    def test_uuid_code_is_not_editable(self):
-        # Not possible to test as editable=False only applies to Django Admin
-        pass
+    def test_name_not_null(self):
+        logger.info('Doing the test_name_not_null test')
+        with self.assertRaises(ValidationError):
+            self.product.name = None
+            self.product.full_clean()  # calls save()
+    
+    #
+    # Attributes from Model
+    #
+    
+    # custom_att
+    def test_custom_att(self):
+        self.assertEqual(self.customer.custom_num, 10)
 
-    def test_uuid_code_unique(self):
-        with self.assertRaises(IntegrityError):
-            ProductCoreFactory.create(uuid_code="c532c472-cb23-4c91-994e-36ca1da8b71e")
-
-    def test_uuid_code_not_null(self):
-        with self.assertRaises(IntegrityError):
-            ProductCoreFactory.create(uuid_code=None)
+    def test_custom_att_null(self):
+        with self.assertRaises(ValidationError):
+            self.customer.custom_num = None
+            self.customer.full_clean()  # calls save()
+    
+    #
+    # Miscellaneous
+    #
+    def test_create_batch(self):
+        CustomerFactory.create_batch(499)
+        self.assertEquals(Customer.objects.count(), 500)  # 499 + setUp one
